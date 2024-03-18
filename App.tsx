4c2e2688900d6c82bc93592d87dc639d010ebf56
ArjunDahal99 <dahalarjun409@gmail.com>
2024-03-18 10:08:47 +0545

    ok

--- App.tsx
+++ App.tsx
@@ -1,13 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
 
-const App = () =>
+
+import * as React from 'react';
+import { View, Text, TouchableOpacity, Pressable, Button, StatusBar } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen()
 {
+  const navigation = useNavigation()
   return (
-    <View className=' bg-white h-screen '>
-      <Text className=' text-black'>App</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title='GG'
+        //@ts-ignore
+        onPress={() => navigation.navigate('Info')}
+      />
+
     </View>
-  )
+  );
 }
 
-export default App
+function InfoScreen()
+{
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <TouchableOpacity>
+        <Text>GG</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App()
+{
+  return (
+    <>
+      <StatusBar
+        hidden
+      />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            options={{ headerBackVisible: false, headerRight: () => (<Button title="Info" color="red" />), }}
+            name="Info" component={InfoScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+export default App;
